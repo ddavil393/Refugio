417e0fae9f2fe721f79f864fa7140b1f85ae6366
AlejandroCedeno06 <ecederlosuarez083@gmail.com>
2024-05-02 10:53:07 -0600

    Detalles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterOutlet} from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, NavbarComponent],
+  imports: [RouterModule, NavbarComponent, HomeComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule} from '@angular/router';
-import { RouterOutlet } from '@angular/router';
-import { AboutComponent } from '../about/about.component';
-import { HomeComponent } from '../home/home.component';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, AboutComponent, HomeComponent],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
