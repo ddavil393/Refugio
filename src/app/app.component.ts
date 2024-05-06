717bcd52b82881c9633ddc263b84a0cc3a374e9f
Diana Dávila <memv.cm@gmail.com>
2024-05-06 00:00:59 -0600

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet} from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, NavbarComponent, HomeComponent, RouterOutlet],
+  imports: [RouterModule, RouterOutlet, FooterComponent, HomeComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
