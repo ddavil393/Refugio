fee889fb0356309aae2928b903c0f19bca89c8a2
AlejandroCedeno06 <ecederlosuarez083@gmail.com>
2024-05-07 12:19:41 -0600

    Pipe Video Seguro

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DomseguroPipe } from '../../domseguro.pipe';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,DomseguroPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+video:string="M8pNE23wP1A?si=mFGUTHVMYtRDEZTF"
 }
