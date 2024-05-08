6bd250f45c5499cace95d894f513d2dba6f48992
Diana Dávila <memv.cm@gmail.com>
2024-05-07 18:38:03 -0600

    changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule} from '@angular/material/grid-list';
 import { RouterOutlet } from '@angular/router';
 import { DomseguroPipe } from '../../domseguro.pipe';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatGridListModule],
-  imports: [RouterOutlet,DomseguroPipe],
+  imports: [MatGridListModule, RouterOutlet, DomseguroPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent{
-
-export class HomeComponent {
-video:string="M8pNE23wP1A?si=mFGUTHVMYtRDEZTF"
+  video:string="M8pNE23wP1A?si=mFGUTHVMYtRDEZTF"
 }
